399d22289b3de126aa812b3f92f71076779945eb
Ummugulsum Cekin <ummugulsumerdim@gmail.com>
2022-11-23 13:52:24 +0300

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Ummugulsum Çekin on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
