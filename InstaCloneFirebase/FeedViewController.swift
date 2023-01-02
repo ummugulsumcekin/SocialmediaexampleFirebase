688f8920efed6ad7c617765d2ac2f566b8eb6734
Ummugulsum Cekin <ummugulsumerdim@gmail.com>
2023-01-02 12:33:38 +0300

    data

--- InstaCloneFirebase/FeedViewController.swift
+++ InstaCloneFirebase/FeedViewController.swift
@@ -8,10 +8,8 @@
 import UIKit
 import Firebase
 
-class FeedViewController : UIViewController , UITableViewDataSource , UITableViewDelegate {
+class FeedViewController : UIViewController , UITableViewDelegate, UITableViewDataSource {
    
-
-    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -31,7 +29,7 @@ class FeedViewController : UIViewController , UITableViewDataSource , UITableVie
         settings.areTimestampsInSnapshotsEnabled = true
         firesStoreDatabase.settings = settings
        */
-        
+    
         firesStoreDatabase.collection("Posts").addSnapshotListener { (snapshot, error) in
             if error != nil {
                 print(error?.localizedDescription)
@@ -46,25 +44,22 @@ class FeedViewController : UIViewController , UITableViewDataSource , UITableVie
                         print(documentID)
                     }
                 }
-                
-                
             }
-        
     }
-        
+    }
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
-        func tableView(_ tableView: UITableView ,cellForRowAtIndexPath indexPath: IndexPath ) -> UITableViewCell {
+    func tableView(_ tableView: UITableView ,cellForRowAt indexPath: IndexPath ) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
             
             cell.userEmailLabel.text = "user@email.com"
             cell.likeLabel.text = "0"
             cell.commentLabel.text = "comment"
             cell.userImageView.image = UIImage(named: "select.png")
-            
             return cell
         }
     
@@ -73,5 +68,4 @@ class FeedViewController : UIViewController , UITableViewDataSource , UITableVie
     }
  
 
-
-    }
+    
